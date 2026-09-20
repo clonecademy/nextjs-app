cbabae516338ddf0fac7025d7d094bd6a360ddf5
Karthik G. Appiah <kar@kar-m1pro.local>
2026-09-19 18:57:13 -0700

    build: init tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import "@/styles/app.css";
+
 export default function RootLayout({
 	children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body className="flex flex-col min-h-svh">{children}</body>
 		</html>
 	);
 }
